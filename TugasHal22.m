3ae0ee13974913c44e1bec97d8adcc8450a4ce2e
alfianhanafii <89461536+alfianhanafii@users.noreply.github.com>
2021-08-25 22:39:39 +0800

    Add files via upload

---
+++ TugasHal22.m
@@ -0,0 +1,25 @@
+12/3.5, (3+5/4)^2
+(0.25^2+0.75^2)^(1/2), 2/(6/0.3)
+A=25;B=50;C=125;D=89;
+X=A+B+C
+Y=A/(D+B)
+Z=D^(A/B)+C
+luas=1
+kel_1=1
+2_data=1
+diff:3=1
+Time=1
+time_from_start=1
+10_hasil_terakhir=1
+nilai-awal
+x=pi/6;y=0.001;
+sqrt(y), exp(-x), sin(x)
+cos(2*x), tan(3*x), log10(y)
+log2(y), log(y)
+p=9+16*i;q=-9+16*i;
+r=p*q, s=p/q, p-r
+r+s, p^2, sqrt(q)
+abs(p), angle(p)
+abs(q), angle(q)
+abs(r), angle(r)
+abs(s), angle(s)
